1d966303ae7ef14a284805d34b9af06ed5eda1f6
Vipin Thakur <vipthaku@cisco.com>
2019-07-18 14:31:23 -0700

    Site implementation

--- src/main/java/com/cisco/dnac/common/constants/DNACUrl.java
+++ src/main/java/com/cisco/dnac/common/constants/DNACUrl.java
@@ -3,4 +3,6 @@ package com.cisco.dnac.common.constants;
 public interface DNACUrl {
 	
 	String SITE_URL = "dna/intent/api/v1/topology/site-topology";
+	String SITE_URL_BY_ID = "dna/intent/api/v1/topology/site-topology";
+	String SITE_URL_BY_TYPE = "dna/intent/api/v1/topology/site-topology";
 }
