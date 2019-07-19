4fad5ef112ec79798e03770f6751582fdcff2691
enataraj <vnselumalai@gmail.com>
2019-07-18 21:26:36 -0700

    Scheduler related changes

--- src/main/java/com/cisco/dnac/scheduler/service/WorkflowInvoker.java
+++ src/main/java/com/cisco/dnac/scheduler/service/WorkflowInvoker.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cisco.dnac.pnp.service.PnpService;
+import com.cisco.dnac.scheduler.db.provider.DNASchedulerDBUtil;
 import com.cisco.dnac.scheduler.dto.SchedulerConstants;
 
 @Service
@@ -12,23 +13,37 @@ public class WorkflowInvoker implements Runnable {
 
 	private static final Logger logger = Logger.getLogger(WorkflowInvoker.class);
 	private String taskName;
+	private int taskId;
 	@Autowired
 	private PnpService pnpServiceInstance;
 
-	public WorkflowInvoker(String taskName) {
+	@Autowired
+	private DNASchedulerDBUtil dbutil;
+
+	public WorkflowInvoker(String taskName, int taskId) {
 		this.taskName = taskName;
+		this.taskId = taskId;
 
 	}
 
 	@Override
 	public void run() {
-		if (SchedulerConstants.PNPSERVICE.equals(taskName)) {
-			logger.info("Invoking PNP Service ... ");
-			pnpServiceInstance.execute();
-		} else {
-			logger.warn("Un Implemented Service .... " + taskName);
-		}
+		try {
+			if (SchedulerConstants.PNPSERVICE.equals(taskName)) {
+				logger.info("Invoking PNP Service ... ");
+				dbutil.updateSchedulerTask(taskId, SchedulerConstants.INPROGRSS);
+				pnpServiceInstance.execute();
+				dbutil.updateSchedulerTask(taskId, SchedulerConstants.COMPLETED);
+			} else {
+				logger.warn("Un Implemented Service .... " + taskName);
+			}
 
+		} catch (Exception e) {
+			logger.error("Exception while running the task ... " + taskName);
+			dbutil.updateSchedulerTask(taskId, SchedulerConstants.FAILED);
+			logger.error("Error ", e);
+
+		}
 	}
 
 }
